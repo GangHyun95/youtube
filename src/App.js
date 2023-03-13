0beb0e785cefd3dd2ae86c1e9ae54e18f35ab31d
hyun <hgh6128@gmail.com>
2023-03-14 05:14:45 +0900

    mock 데이터를 이용한 데이터 통신

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Outlet } from "react-router-dom";
+import SearchHeader from "./components/SearchHeader";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
     <>
-      <Outlet />
+      <SearchHeader />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,5 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { useParams } from "react-router-dom";
+import VideoCard from "../components/VideoCard";
+import { search } from "../api/youtube";
+import FakeYoutube from "../api/fakeYoutube";
 
 export default function Videos() {
-  return <div>Videos</div>;
+  const { keyword } = useParams();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["videos", keyword], () => {
+    const youtube = new FakeYoutube();
+    return youtube.search(keyword);
+  });
+  return (
+    <>
+      <div>Videos {keyword ? `🏸${keyword}` : `💥`}</div>;
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong 😥</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
