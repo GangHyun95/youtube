63cbc6b6f7cff3f616a2ea5d8307a371a20288fc
hyun <hgh6128@gmail.com>
2023-03-26 14:40:34 +0900

    router basename 설정

--- src/index.js
+++ src/index.js
@@ -8,19 +8,22 @@ import NotFound from "./pages/NotFound";
 import Videos from "./pages/Videos";
 import VideoDetail from "./pages/VideoDetail";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Videos /> },
-      { path: "videos", element: <Videos /> },
-      { path: "videos/:keyword", element: <Videos /> },
-      { path: "videos/watch/:videoId", element: <VideoDetail /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <NotFound />,
+      children: [
+        { index: true, element: <Videos /> },
+        { path: "videos", element: <Videos /> },
+        { path: "videos/:keyword", element: <Videos /> },
+        { path: "videos/watch/:videoId", element: <VideoDetail /> },
+      ],
+    },
+  ],
+  { basename: "/youtube" }
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
